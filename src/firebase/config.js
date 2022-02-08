b697de1a1fcb8c2df9a68627a97c1b0d3388929b
Jordan Mowry <jordan.mowry@gmail.com>
2022-02-08 12:00:28 -0700

    got config file set up for firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+// Firebase configuration object
+const firebaseConfig = {
+  apiKey: process.env['REACT_APP_API-KEY'],
+  authDomain: process.env['REACT_APP_AUTH-DOMAIN'],
+  projectId: process.env['REACT_APP_PROJECT-ID'],
+  storageBucket: process.env['REACT_APP_STORAGE-BUCKET'],
+  messagingSenderId: process.env['REACT_APP_MESSAGING-SENDER-ID'],
+  appId: process.env['REACT_APP_APP-ID'],
+};
+
+// initialize app
+firebase.initializeApp(firebaseConfig);
+
+// initialize services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+// timestamp
+const timestamp = firebase.firestore.Timestamp;
+
+// export firebase services
+export { projectFirestore, projectAuth, timestamp };
