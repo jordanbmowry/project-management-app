c1194fc3dbd2202c1c80af5aa7da0803e0c6387b
Jordan Mowry <jordan.mowry@gmail.com>
2022-02-10 18:04:10 -0700

    got basic routes set up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// styles
 import './App.css';
+
+// pages and components
+import Dashboard from './pages/dashboard/Dashboard';
+import Create from './pages/create/Create';
+import Login from './pages/login/Login';
+import Project from './pages/project/Project';
+import Signup from './pages/signup/Signup';
 
 function App() {
   console.log(process.env);
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Router>
+        <div className='container'>
+          <Switch>
+            <Route exact path='/'>
+              <Dashboard />
+            </Route>
+            <Route exact path='/create'>
+              <Create />
+            </Route>
+            <Route exact path='/projects/:id'>
+              <Project />
+            </Route>
+            <Route exact path='/login'>
+              <Login />
+            </Route>
+            <Route exact path='/signup'>
+              <Signup />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/create/Create.js
@@ -0,0 +1,5 @@
+import styles from './Create.module.css';
+
+export default function Create() {
+  return <div>Create</div>;
+}
