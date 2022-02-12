30bf4109d7c99286d6e4fa6d30711706317e108b
Jordan Mowry <jordan.mowry@gmail.com>
2022-02-12 12:53:26 -0700

    changed the useSignup hook to take in profile image

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import 'firebase/storage';
 // Firebase configuration object
 const firebaseConfig = {
   apiKey: process.env['REACT_APP_API-KEY'],
@@ -17,9 +18,10 @@ firebase.initializeApp(firebaseConfig);
 // initialize services
 const projectFirestore = firebase.firestore();
 const projectAuth = firebase.auth();
+const projectStorage = firebase.storage();
 
 // timestamp
 const timestamp = firebase.firestore.Timestamp;
 
 // export firebase services
-export { projectFirestore, projectAuth, timestamp };
+export { projectFirestore, projectAuth, timestamp, projectStorage };
