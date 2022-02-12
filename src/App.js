4b97faa29649fd9291dcd6f03566bc19afbe1e62
Jordan Mowry <jordan.mowry@gmail.com>
2022-02-12 11:43:17 -0700

    created Sidebar and styled component

--- src/App.js
+++ src/App.js
@@ -9,12 +9,14 @@ import Login from './pages/login/Login';
 import Project from './pages/project/Project';
 import Signup from './pages/signup/Signup';
 import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
 
 function App() {
   console.log(process.env);
   return (
     <div className='App'>
       <Router>
+        <Sidebar />
         <div className='container'>
           <Navbar />
           <Switch>
---
+++ src/components/Sidebar.js
@@ -0,0 +1,34 @@
+import { NavLink } from 'react-router-dom';
+//styles and images
+import styles from './Sidebar.module.css';
+import DashboardIcon from '../assets/dashboard_icon.svg';
+import AddIcon from '../assets/add_icon.svg';
+
+export default function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <div className={styles['sidebar-content']}>
+        <div className={styles.user}>
+          {/* TODO: avatar and username later */}
+          <p>Hey user</p>
+        </div>
+        <nav className={styles.links}>
+          <ul>
+            <li>
+              <NavLink className={styles.active} exact to='/'>
+                <img src={DashboardIcon} alt='dashboard icon'></img>
+                <span>Dashboard</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className={styles.active} to='/create'>
+                <img src={AddIcon} alt='add project icon'></img>
+                <span>New Project</span>
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+}
