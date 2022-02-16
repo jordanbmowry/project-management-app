4288405de07f95351993be66b634c9a1d13f2920
Jordan Mowry <jordan.mowry@gmail.com>
2022-02-15 17:29:21 -0700

    create basic form for Create.js

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,5 +1,68 @@
+import { useState } from 'react';
+// styles
 import styles from './Create.module.css';
 
 export default function Create() {
-  return <div>Create</div>;
+  const initialState = {
+    name: '',
+    details: '',
+    dueDate: '',
+    category: '',
+  };
+
+  const [formState, setFormState] = useState(initialState);
+  const [assignedUsers, setAssignedUsers] = useState([]);
+
+  const handleFormChange = ({ target }) => {
+    const { value, name } = target;
+    setFormState((prevFormState) => ({
+      ...prevFormState,
+      [name]: value,
+    }));
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(formState);
+  };
+
+  return (
+    <div className={styles['create-form']}>
+      <h2 className='page-title'>Create a new project</h2>
+      <form onSubmit={handleFormSubmit}>
+        <label htmlFor='name'>Project name:</label>
+        <input
+          type='text'
+          name='name'
+          id='name'
+          value={formState.name}
+          onChange={handleFormChange}
+          required
+        />
+        <label htmlFor='details'>Project details:</label>
+        <textarea
+          type='text'
+          name='details'
+          id='details'
+          onChange={handleFormChange}
+          value={formState.details}
+          required
+        />
+        <label htmlFor='dueDate'>Set due date:</label>
+        <input
+          type='date'
+          id='dueDate'
+          name='dueDate'
+          value={formState.dueDate}
+          onChange={handleFormChange}
+          required
+        />
+        <label htmlFor='category'>Project category:</label>
+        {/* TODO: */}
+        <label htmlFor='AssignTo'>Assign to:</label>
+        {/* TODO: */}
+        <button className='btn'>Add Project</button>
+      </form>
+    </div>
+  );
 }
